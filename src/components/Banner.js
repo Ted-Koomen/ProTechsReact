0b81845a2456452cf173ad46ef25c94ec8aad3c5
Ted-Koomen <koomen.ted@gmail.com>
2017-09-03 13:06:02 -0400

    add banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { PageHeader } from 'react-bootstrap';
+import '../index.css'
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>
+          Pro-Techs Pest Control
+        </h1>
+      </div>
+    )
+  }
+}
+
+
+export default Banner
